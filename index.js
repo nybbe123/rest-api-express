05ea73eb02d4b019ddd011f99c8b8a4b9ed509c2
Jonatan Nyberg <33462941+nybbe123@users.noreply.github.com>
2022-04-30 11:51:46 +0200

    saving data in json file

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-const shoes = [
-    {
-        id: '1',
-        brand: 'Vans',
-        type: 'Street',
-        price: 99,
-    },
-    {
-        id: '2',
-        brand: 'New Balance',
-        type: 'Streetrunning',
-        price: 99,
-    },
-    {
-        id: '3',
-        brand: 'Hoka One One',
-        type: 'Trailrunning',
-        price: 99,
-    }
-]
+// const shoes = [
+//     {
+//         id: '1',
+//         brand: 'Vans',
+//         type: 'Street',
+//         price: 99,
+//     },
+//     {
+//         id: '2',
+//         brand: 'New Balance',
+//         type: 'Streetrunning',
+//         price: 99,
+//     },
+//     {
+//         id: '3',
+//         brand: 'Hoka One One',
+//         type: 'Trailrunning',
+//         price: 99,
+//     }
+// ]
 
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const uniqId = require('uniqid');
+
+let jsonData = fs.readFileSync('data.json');
+let shoes = JSON.parse(jsonData);
+
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -44,7 +50,7 @@ app.get('/shoes/:id', (req, res) => {
 
 app.post('/shoes', (req, res) => {
     const newShoe  = {
-        id: shoes.length + 1,
+        id: uniqId(),
         brand: req.body.brand,
         type: req.body.type,
         price: req.body.price,
